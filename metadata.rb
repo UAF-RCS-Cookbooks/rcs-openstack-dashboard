be361a6a7f303f6e7e56e5663900185b41e3d20f
Jeremiah Dabney <jdabney@gmail.com>
2025-04-09 22:11:01 -0800

    Bumped version.

--- metadata.rb
+++ metadata.rb
@@ -3,14 +3,14 @@ maintainer       'UAF RCS'
 maintainer_email 'chef@rcs.alaska.edu'
 license          'Apache-2.0'
 description      'Installs/Configures the OpenStack Dashboard (Horizon)'
-version          '20.0.0'
+version          '20.0.1'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
 end
 
-depends 'apache2', '~> 8.6'
-depends 'rcs-openstack-common', '>= 20.0.0'
-depends 'rcs-openstack-identity', '>= 20.0.0'
+depends 'apache2', '~> 9.3.6'
+depends 'rcs-openstack-common'
+depends 'rcs-openstack-identity'
 
 chef_version '>= 16.0'
